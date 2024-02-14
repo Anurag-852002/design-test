1427ed39cfd13e652aac65552d583694b3ec6976
Anurag-852002 <anurag842002@gmail.com>
2024-02-14 14:18:23 +0530

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+const EpicGames = () => {
+  return (
+    <div className="epic-games-container">
+      <header className="epic-games-header">
+        {/* Add your logo, buttons, and icons here */}
+      </header>
+      <main className="epic-games-features">
+        {/* Add your feature sections here */}
+      </main>
+      <footer className="epic-games-footer">
+        {/* Add your copyright information here */}
+      </footer>
+    </div>
+  );
+};
+
+export default EpicGames;
